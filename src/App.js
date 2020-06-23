7d7aa4308e9829dcb5eae3943309c3f7131b259b
jaovito <junior.vitorio.dias@gmail.com>
2020-06-23 16:06:35 -0300

    Add files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import GlobalStyle from './styles/global'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import api from './services/api'
+
+import {uniqueId} from 'lodash' // lib para gerar id unico
+import filesize from 'filesize' // lib para demonstrar o tamanho do arquivo e mostrar se é mb, kb, gb, etc.
+
+import { Container, Content} from './styles'
+import Upload from './components/Upload'
+
+import FileList from './components/FileList'
+
+export default class App extends Component {
+  state = {
+    uploadedFiles: [],
+  }
+
+  async componentDidMount() {
+    const response = await api.get('posts')
+
+    this.setState({
+      uploadedFiles: response.data.map(file => ({
+        id: file._id,
+        name: file.name,
+        readableSize: filesize(file.size),
+        preview: file.url,
+        uploaded: true,
+        url: file.url,
+      }))
+    })
+  }
+
+  handleUpload = files => {
+    const uploadedFiles = files.map(file => ({
+      file,
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview: URL.createObjectURL(file),
+      progress: 0,
+      uploaded: false,
+      error: false,
+      url: null,
+    }))
+
+    this.setState({ uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)})
+  
+    uploadedFiles.forEach(this.processUpload)
+  }
+
+  updateFile = (id, data) => {
+    this.setState({ uploadedFiles: this.state.uploadedFiles.map(uploadedFile => {
+      return id === uploadedFile.id ? {...uploadedFile, ...data} : uploadedFile
+    })})
+  }
+
+  processUpload = (uploadedFile) => {
+    const data = new FormData()
+
+    data.append('file', uploadedFile.file, uploadedFile.name) // aqui o 'file' é o nome que demos no backend, colocamos file e passamos um arquivo.
+
+    api.post('posts', data, {
+      onUploadProgress: e => {
+        const progress = parseInt(Math.round((e.loaded * 100) / e.total))
+
+        this.updateFile(uploadedFile.id, {
+          progress,
+        })
+      }
+    }).then((response) => {
+      this.updateFile(uploadedFile.id, {
+        uploaded: true,
+        id: response.data._id,
+        url: response.data.url
+      })
+    }).catch(() => {
+      this.updateFile(uploadedFile.id, {
+        error: true
+      })
+    })
+  }
+
+  handleDelete = async id => {
+    await api.delete(`posts/${id}`)
+
+    this.setState({ 
+      uploadedFiles: this.state.uploadedFiles.filter(file => file.id !== id)
+     })
+  }
+
+  componentWillUnmount() {
+    this.state.uploadedFiles.forEach(file => URL.revokeObjectURL(file.preview))
+  }
+
+  render() {
+    const {uploadedFiles} = this.state
+  
+    return (
+      <Container>
+
+       <Content>
+         <Upload onUpload={this.handleUpload} />
+         
+        { !!uploadedFiles.length && (<FileList files={uploadedFiles} onDelete={this.handleDelete} />)}
+       </Content>
+
+       <GlobalStyle />
+
+      </Container>
+    )
+  }
 }
-
-export default App;
